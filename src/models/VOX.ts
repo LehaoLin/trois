e6b2af38384fa95fa246606ac4def9d43c4c5383
LehaoLin <lehaolin@link.cuhk.edu.cn>
2022-04-19 14:14:02 +0800

    update

--- src/models/VOX.ts
+++ src/models/VOX.ts
@@ -1,23 +1,16 @@
-import { defineComponent } from 'vue'
-import { VOXLoader, VOXMesh } from 'three/examples/jsm/loaders/VOXLoader.js'
-import Model from './Model'
+// import { defineComponent } from 'vue'
+// import { VOXLoader, VOXMesh } from 'three/examples/jsm/loaders/VOXLoader.js'
+// import Model from './Model'
+// import {VoxelLoader} from 'three-voxel-loader'
 
-export default defineComponent({
-  extends: Model,
-  created() {
-    const loader = new VOXLoader()
-    this.$emit('before-load', loader)
-    loader.load(this.src, (chunks) => {
-        for ( let i = 0; i < chunks.length; i ++ ) {
-            const chunk = chunks[ i ];
-            // displayPalette( chunk.palette );
-            const mesh = new VOXMesh( chunk );
-            mesh.scale.setScalar( 0.0015 );
-            this.onLoad(mesh)
-            this.initObject3D(mesh)
-        }
-    //   this.onLoad(vox)
-    //   this.initObject3D(vox)
-    }, this.onProgress, this.onError)
-  },
-})
+// export default defineComponent({
+//   extends: Model,
+//   created() {
+//     const loader = new VoxelLoader()
+//     this.$emit('before-load', loader)
+//     loader.load(this.src, (vox) => {
+//       this.onLoad(vox)
+//       this.initObject3D(vox)
+//     }, this.onProgress, this.onError)
+//   },
+// })
