344dc334f14f34c884ae7a9c293dd672a7de0d8a
LehaoLin <lehaolin@link.cuhk.edu.cn>
2022-04-19 13:53:53 +0800

    debug and rebuild for vox

--- src/models/VOX.ts
+++ src/models/VOX.ts
@@ -1,5 +1,5 @@
 import { defineComponent } from 'vue'
-import { VOXLoader } from 'three/examples/jsm/loaders/VOXLoader.js'
+import { VOXLoader, VOXMesh } from 'three/examples/jsm/loaders/VOXLoader.js'
 import Model from './Model'
 
 export default defineComponent({
@@ -7,9 +7,17 @@ export default defineComponent({
   created() {
     const loader = new VOXLoader()
     this.$emit('before-load', loader)
-    loader.load(this.src, (vox) => {
-      this.onLoad(vox)
-      this.initObject3D(vox)
+    loader.load(this.src, (chunks) => {
+        for ( let i = 0; i < chunks.length; i ++ ) {
+            const chunk = chunks[ i ];
+            // displayPalette( chunk.palette );
+            const mesh = new VOXMesh( chunk );
+            mesh.scale.setScalar( 0.0015 );
+            this.onLoad(mesh)
+            this.initObject3D(mesh)
+        }
+    //   this.onLoad(vox)
+    //   this.initObject3D(vox)
     }, this.onProgress, this.onError)
   },
 })
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,3 @@
 export { default as GltfModel } from './GLTF'
 export { default as FbxModel } from './FBX'
-export { default as VoxModel } from './VOX'
+export { default as VoxModel } from './Vox'
